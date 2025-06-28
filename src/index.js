57a70e7e7b0fc70ba36c5b05e92800615d47b630
Mohd Subhan <mdsammlk00@gmail.com>
2025-06-28 10:36:12 +0530

    update

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ app.set("view engine", "hbs");
 app.set("views", templatesPath);
 
 // Middleware
+
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
